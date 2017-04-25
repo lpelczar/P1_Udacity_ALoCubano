443e6016c55cf23f78fb1667335e1cf4ac78b8e6
lpelczar <lukepelczar@gmail.com>
2017-04-25 14:06:20 +0200

    Initial commit

---
+++ app/src/main/java/pl/lukaszpelczar/casino/MainActivity.java
@@ -0,0 +1,13 @@
+package pl.lukaszpelczar.casino;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
